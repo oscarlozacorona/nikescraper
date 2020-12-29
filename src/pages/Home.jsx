3480b79d23ab510108c732bc2dd69caea2ede399
Oscar Loza-Corona <oscar@Oscars-MacBook-Pro-2.local>
2020-12-29 12:05:23 -0800

    finished home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 // Requirements
 import React from "react";
-import { withRouter } from "react-router-dom";
-import offWhiteShoes from "../images/offwhite.jpeg";
+import { withRouter, Link } from "react-router-dom";
 // css
 import "../css/pages/home.css";
 
@@ -9,13 +8,31 @@ import "../css/pages/home.css";
 const Home = () => (
     <div className='container d-flex justify-content-center home_container'>
         <div className='col-12 justify-content-center'>
+            <div className='d-flex text-center p-5'>
+                <h1 className='text-white home_main_text'>
+                    Welcome
+                    <span>😈</span>
+                </h1>
+            </div>
             <div class='row center'>
-                <section class='cardone home_card'>
-                    <img class="logo" src="https://static.dealwiki.net/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/1/0/1000px-logo_nike.svg.png" />
-                </section>
-                <section class='cardtwo home_card'>
-                    <img class="logo" src="https://static.dealwiki.net/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/1/0/1000px-logo_nike.svg.png" />
-                </section>
+                <button class='cardone home_card'>
+                    <h4 className='text-dark'>Get Shoe Options</h4>
+                    <img
+                        class='logo'
+                        src='https://static.dealwiki.net/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/1/0/1000px-logo_nike.svg.png'
+                        alt='nike logo'
+                    />
+                </button>
+                <button class='cardtwo home_card'>
+                    <Link to='/buy'>
+                        <h4 className='text-white'>Purchase Shoes</h4>
+                        <img
+                            class='logo'
+                            src='https://static.dealwiki.net/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/1/0/1000px-logo_nike.svg.png'
+                            alt='nike logo'
+                        />
+                    </Link>
+                </button>
             </div>
         </div>
     </div>
